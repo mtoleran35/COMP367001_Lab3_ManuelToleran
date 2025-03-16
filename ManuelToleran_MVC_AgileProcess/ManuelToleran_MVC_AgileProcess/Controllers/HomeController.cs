9df1e807af311343f7de40553d3f7eca89c52e3d
Manuel Toleran <mtoleran@my.centennialcollege.ca>
2025-03-16 17:38:24 -0400

    Initial commit

---
+++ ManuelToleran_MVC_AgileProcess/ManuelToleran_MVC_AgileProcess/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using ManuelToleran_MVC_AgileProcess.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ManuelToleran_MVC_AgileProcess.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ManuelToleran_MVC_AgileProcess/ManuelToleran_MVC_AgileProcess/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ManuelToleran_MVC_AgileProcess.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
